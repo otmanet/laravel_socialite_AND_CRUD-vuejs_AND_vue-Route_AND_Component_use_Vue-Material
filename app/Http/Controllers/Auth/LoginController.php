d9b604783860080c822324e68fcc371f94058353
otmanet <37757213+otmanet@users.noreply.github.com>
2021-10-28 00:02:56 +0100

    change every thing for new test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
@@ -38,45 +40,69 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-    //Google login:
-    public function redirectToGoogle(){
+     // Google login
+     public function redirectToGoogle()
+     {
+     return Socialite::driver('google')->redirect();
+     }
 
-        return Socialite::driver('google')->redirect();
+     // Google callback
+     public function handleGoogleCallback()
+     {
+     $user = Socialite::driver('google')->user();
 
-    }
+     $this->_registerOrLoginUser($user);
 
-    //Google callback:
-    public function handleGoogleCallback(){
+     // Return home after login
+     return redirect()->route('home');
+     }
 
-        $user= Socialite::driver('google')->user();
-
-    }
-
-     //Facebook login:
-     public function redirectToFacebook(){
-
+     // Facebook login
+     public function redirectToFacebook()
+     {
      return Socialite::driver('facebook')->redirect();
-
      }
 
-     //Facebook callback:
-     public function handleFacebookCallback(){
+     // Facebook callback
+     public function handleFacebookCallback()
+     {
+     $user = Socialite::driver('facebook')->user();
 
-     $user= Socialite::driver('facebook')->user();
+     $this->_registerOrLoginUser($user);
 
+     // Return home after login
+     return redirect()->route('home');
      }
 
-       //Github login:
-       public function redirectToGithub(){
+     // Github login
+     public function redirectToGithub()
+     {
+     return Socialite::driver('github')->redirect();
+     }
 
-       return Socialite::driver('github')->redirect();
+     // Github callback
+     public function handleGithubCallback()
+     {
+     $user = Socialite::driver('github')->user();
 
-       }
+     $this->_registerOrLoginUser($user);
 
-       //Facebook callback:
-       public function handleGithubCallback(){
+     // Return home after login
+     return redirect()->route('home');
+     }
 
-       $user= Socialite::driver('github')->user();
+     protected function _registerOrLoginUser($data)
+     {
+     $user = User::where('email', '=', $data->email)->first();
+     if (!$user) {
+     $user = new User();
+     $user->name = $data->name;
+     $user->email = $data->email;
+     $user->provider_id = $data->id;
+     $user->avatar = $data->avatar;
+     $user->save();
+     }
 
-       }
+     Auth::login($user);
+     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,17 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Google login:
-Route::get('/login/google', [App\Http\Controllers\Auth\LoginController::class,'redirectToGoogle'])->name('login.google');
-Route::get('/login/google/callback', [App\Http\Controllers\Auth\LoginController::class,'handleGoogleCallback']);
+// Google login
+Route::get('login/google', [App\Http\Controllers\Auth\LoginController::class,
+'redirectToGoogle'])->name('login.google');
+Route::get('login/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']);
 
-//facebook login:
-Route::get('/login/facebook',[App\Http\Controllers\Auth\LoginController::class,'redirectToFacebook'])->name('login.facebook');
-Route::get('/login/facebook/callback', [App\Http\Controllers\Auth\LoginController::class,'handleFacebookCallback']);
+// Facebook login
+Route::get('login/facebook', [App\Http\Controllers\Auth\LoginController::class,
+'redirectToFacebook'])->name('login.facebook');
+Route::get('login/facebook/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleFacebookCallback']);
 
-//github login:
-Route::get('/login/github',[App\Http\Controllers\Auth\LoginController::class,'redirectToGithub'])->name('login.github');
-Route::get('/login/github/callback', [App\Http\Controllers\Auth\LoginController::class,'handleGithubCallback']);
+// Github login
+Route::get('login/github', [App\Http\Controllers\Auth\LoginController::class,
+'redirectToGithub'])->name('login.github');
+Route::get('login/github/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGithubCallback']);
